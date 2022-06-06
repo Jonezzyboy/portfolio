5789c1ab24f1cb3de73ed359415a07db3772a3b1
Alan Jones <jonezzyboy@gmail.com>
2022-06-06 22:44:58 +0100

    Added default About Page

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import Layout from '../components/Layout'
+
+export default function About() {
+    return (
+      <Layout>
+        <main>
+  
+          <title>Alan Jones: About</title>
+  
+        </main>
+      </Layout>
+    )
+  }
