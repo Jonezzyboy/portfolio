f981cac568a6d8420655a3e64f2723e63dc7b82a
Alan Jones <jonezzyboy@gmail.com>
2022-06-06 22:25:04 +0100

    Navigation Styling Tweaks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Navbar() {
     return (
         <nav>
-            <h1>Alan Jones - Portfolio</h1>
+            <h1>Alan Jones</h1>
             <div className="links">
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
